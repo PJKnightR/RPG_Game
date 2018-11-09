aeae5153a7d363a7bbd2c41fbacc20ebd62731e7
ORAMPM17 <ORAMPM17@5CG73105SY.GCC.edu>
2018-11-09 16:05:36 -0500

    added basic methods to enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,3 +1,33 @@
-public class Enemy extends Actor {
-    //handles enemy exclusive elements
+package game;
+
+abstract public class Enemy extends Actor {
+	
+	public void health() {
+		health = 100;
+	}
+	
+	public void defence() {
+		defense = 50;
+	}
+	
+	public void attack() {
+		attack = 50;
+	}
+	
+	//Damage is calculated in Battle
+	public void damage() {
+		//damage = (((((2 * level)/5)+2)*power*(attack/defence))/50)+2; 
+		//What would Power be?
+	}
+	
+	//Speed is determined in Battle
+	public void speed() {
+		speed = 1.0;
+	}
+	
+	// Should set the enemy level based on the player level
+	// Currently sets a base level
+	public void setLevel() {
+		level = 1;
+	}
 }
