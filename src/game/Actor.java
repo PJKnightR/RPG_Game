becdcea24f3f6a75f3689038ea0de297b37aa626
YORKWT17 <YORKWT17@5CG73108HZ.GCC.edu>
2018-11-09 17:09:05 -0500

    dfghj

--- src/game/Actor.java
+++ src/game/Actor.java
@@ -13,33 +13,49 @@ abstract public class Actor {
 	//protected ArrayList<item.Item> Inventory = new ArrayList<>();
 	//protected String Class;
 
-
+	/**
+	 * This method gets Name
+	 */
 	protected abstract void getName();
-	
+	/**
+	 * This method gets Health
+	 */
 	protected  void getHealth() {
 		
 	}
-	
+	/**
+	 * This method gets Health Left
+	 */
 	protected void getHealthLeft() {
 		
 	}
-	
+	/**
+	 * This method gets Defence
+	 */
 	protected  void getDefence() {
 		
 	}
-	
+	/**
+	 * This method gets attack
+	 */
 	protected  void getAttack() {
 		
 	}
-	
+	/**
+	 * This method gets Damage
+	 */
 	protected  void getDamage() {
 		
 	}
-	
+	/**
+	 * This method gets Speed
+	 */
 	protected  void getSpeed() {
 		
 	}
-	
+	/**
+	 * This method gets Level
+	 */
 	protected  void getLevel() {
 		
 	}
