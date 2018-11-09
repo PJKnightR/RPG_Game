2ed943ba743943b9b255a2ec9e34e03eb71cf01c
YORKWT17 <YORKWT17@5CG73108HZ.GCC.edu>
2018-11-09 16:58:06 -0500

    abstract methods added

--- src/game/Actor.java
+++ src/game/Actor.java
@@ -16,4 +16,20 @@ abstract public class Actor {
     /**
      * Add the basic get() methods to this class
      */
+	
+	protected abstract void getName();
+	
+	protected abstract void getHealth();
+	
+	protected abstract void getHealthLeft();
+	
+	protected abstract void getDefence();
+	
+	protected abstract void getAttack();
+	
+	protected abstract void getDamage();
+	
+	protected abstract void getSpeed();
+	
+	protected abstract void getLevel();
 }
